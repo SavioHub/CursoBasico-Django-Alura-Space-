c83401ede924e8317a8d7414b1f517ac7e0eb513
savioHub <saviosantossilva19@gmail.com>
2022-12-13 22:20:24 -0300

    quase finalizando

--- galeria/urls.py
+++ galeria/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from galeria.views import index
 #importação do index dentro do app galeria no arquivo viwes.py
 urlpatterns = [
-    path('', index),
-    # path('imagem/', imagem),
+    path('', index, name='index'),
+    # path('imagem/', imagem, name='imagem'),
 ]
